4d26b92dc40c566cf4448e5462cdf4407d75ae1f
Goncalo Ramalho <meet@goncaloramalho.com>
2020-10-05 16:53:34 +0100

    Add info to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,13 @@
+import { getCurrentYear } from 'helpers/helpers';
 import React from 'react';
 import './footer.scss';
 
 const Footer = (): JSX.Element => {
-  return <footer className="footer">copyright</footer>;
+  return (
+    <footer className="footer">
+      created by Gonçalo Ramalho {getCurrentYear()}
+    </footer>
+  );
 };
 
 export default Footer;
