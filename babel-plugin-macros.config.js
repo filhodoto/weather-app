b8de68d62aaa636dffdeb0f5b68dc45cbfdf9c19
Goncalo Ramalho <meet@goncaloramalho.com>
2020-11-10 11:44:42 +0000

    Improve develop process - Add prettier locally - Add husky and pre-lint - Run prettier before commit

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // Set naming convention without parent for better reading while inspecting.
-  // Ex: Header__HeaderStyled-sc-63yajg-0 ---> HeaderStyled-sc-63yajg-0  
+  // Ex: Header__HeaderStyled-sc-63yajg-0 ---> HeaderStyled-sc-63yajg-0
   styledComponents: {
-      displayName: true,
-      fileName: false,
-    }
-  };
+    displayName: true,
+    fileName: false,
+  },
+};
